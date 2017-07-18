09c9dd6f03d469ff941e1991ec74e634a00628e4
rashky33 <rashky33@yahoo.com>
2017-07-18 19:29:11 +0300

    adding sounds

---
+++ 1.1.0-install-sound-v1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+##################################################################################################################
+# This will be next to xorg installation
+##################################################################################################################
+
+#Sound
+sudo pacman -S pulseaudio pulseaudio-alsa pavucontrol  --noconfirm --needed
+sudo pacman -S alsa-utils alsa-plugins alsa-lib alsa-firmware --noconfirm --needed
+sudo pacman -S gst-plugins-good gst-plugins-bad gst-plugins-base gst-plugins-ugly  gstreamer  --noconfirm --needed
+sudo pacman -S mplayer vlc pragha  --noconfirm --needed
+
+
+
+echo "################################################################"
+echo "##########   sound drivers has been installed   ################"
+echo "################################################################"
+
