a4976628a332b7e58b440640d614e5a674e3b07e
rashky33 <rashky33@yahoo.com>
2017-07-19 07:20:50 +0300

    editing system

--- 1.0.0-system.sh
+++ 1.0.0-system.sh
@@ -5,14 +5,16 @@ set -e
 ##################################################################################################################
 
 #System
+sudo pacman -Syu
+
 sudo pacman-key  --init
 sudo pacman-key --populate archlinux
 sudo pacman -S multilib-devel fakeroot git jshon wget make pkg-config autoconf automake patch --noconfirm --needed
 sudo pacman -S xdg-user-dirs htop mlocate ntp hplip ifplugd modemmanager usb_modeswitch --noconfirm --needed
-sudo pacman -S termite xcompmgr guvcview --noconfirm --needed
+sudo pacman -S termite xcompmgr --noconfirm --needed
 sudo pacman -S vim   --noconfirm --needed
 
-
+sudo systemctl enableModemManager.service
 
 echo "################################################################"
 echo "##########   system drivers has been installed   ###############"
